c12f2a9f8571d945e216ff743a89efa852c87e40
anawl <anawl@LAPTOP-QL8N4L2N>
2023-06-21 21:48:56 -0300

    semi final commit

---
+++ Arquivos/src/Main.java
@@ -0,0 +1,17 @@
+import persistencia.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Gravacao g = new Gravacao();
+		g.gravarConcatString("kaka1");
+		g.gravarConcatString("kaka2");
+		
+		
+		Leitura l = new Leitura();
+		
+	}
+
+}
---
+++ Arquivos/src/persistencia/Leitura.java
@@ -0,0 +1,66 @@
+package persistencia;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Leitura {
+	
+	private BufferedReader leitor;
+	private String diretorio;
+	
+	public Leitura() {
+		diretorio = "src\\arquivo\\Dados.txt";
+		
+//		try {
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		
+	}
+	
+	public String[] lerLinhas() {
+		
+		ArrayList<String> arrayStringTemp = new ArrayList<String>();
+		
+		try {
+			leitor = new BufferedReader(new FileReader(diretorio));
+			while (leitor.ready()) {
+				arrayStringTemp.add(leitor.readLine());
+			}
+			leitor.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		String arrayString[] = new String[arrayStringTemp.size()];
+		int cont =0;
+		for (String str : arrayStringTemp) {
+			arrayString[cont] = str;
+		}
+		
+		return arrayString;
+	}
+
+	
+	 protected String lerConcatString() {
+		String str = "";
+		try {
+			leitor = new BufferedReader(new FileReader(diretorio));
+			while (leitor.ready()) {
+				str += (leitor.readLine()+"\n");
+			}
+			leitor.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return str;
+	}
+	
+	// String[] strTemp = leitor.readLine().split(";");
+	
+		
+	
+}
